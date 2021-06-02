1aeae61f6a916eeb59ae81f77da6f59c43345949
narekzamanyan <63209701+narekzamanyan@users.noreply.github.com>
2021-06-02 16:00:03 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
     public static void main(String[] args ) {
-        Circle c1 = new Circle("blue", true, 7.7);
-
 
         Circle []circles = new Circle[10];
         for (int i=0; i<10; i++){
