9cbde1a9e06e7b220676be56b015858065883caf
narekzamanyan <63209701+narekzamanyan@users.noreply.github.com>
2021-06-03 15:20:20 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ public class Main {
 
         Shape []shapes = new Shape[10];
         for (int i=0; i<10; i++) {
-            if(i%2 == 0 )     { shapes[i] = new Circle(2.0*(i+1)); }
-            else if(i%3 == 0) { shapes[i] = new Rectangle(2.0*(i+1), 3); }
-            else              { shapes[i] = new Square(2.0*(i+1)); }
+            if(i%2==0)      { shapes[i] = new Circle(2.0*(i+1)); }
+            else if(i%3==0) { shapes[i] = new Rectangle(2.0*(i+1), 3); }
+            else            { shapes[i] = new Square(2.0*(i+1)); }
         }
 
         for (int i =0; i<10; i++) {
